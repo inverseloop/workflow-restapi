abf8bc43b820b2bc62f524c294cf29a2c20fb9e3
whipjack <robderr@gmail.com>
2018-10-12 13:31:59 -0400

    refine test

--- restapi/tests/server.index.test.js
+++ restapi/tests/server.index.test.js
@@ -10,7 +10,12 @@ describe("routes: index", () => {
   test("should respond as expected", async () => {
     const response = await request(server).get("/");
     expect(response.status).toEqual(200);
+	  console.log('response.status = '+ response.status)
     expect(response.type).toEqual("application/json");
-    expect(response.body.data).toEqual("Sending some JSON");
+	  console.log('response.type= '+ response.type)
+    expect(response.body.message).toEqual("ok");
+    expect(response.body.status).toEqual("success");
+	  console.log('response.body.message = '+ response.body.message)
+	  console.log('response.body.status = '+ response.body.status)
   });
 });
